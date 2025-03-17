a30798ddc43d9ca7b19cea42e9ea15032912ccbf
Jeremy Eberle <jwe4ec@virginia.edu>
2025-03-17 11:22:33 -0500

    Prepare data for prediction WIP

---
+++ 03_ml/code/01_prepare_data_for_prediction.R
@@ -0,0 +1,65 @@
+# ---------------------------------------------------------------------------- #
+# Prepare Data for Prediction Models -----
+# Author: Jeremy W. Eberle
+# ---------------------------------------------------------------------------- #
+
+# ---------------------------------------------------------------------------- #
+# Notes ----
+# ---------------------------------------------------------------------------- #
+
+# Before running script, restart R (CTRL+SHIFT+F10 on Windows) and set working 
+# directory to parent folder
+
+# ---------------------------------------------------------------------------- #
+# Store working directory, check correct R version, load packages ----
+# ---------------------------------------------------------------------------- #
+
+# Store working directory
+
+wd_dir <- getwd()
+
+# Load custom functions
+
+source("./02_networks/code/01_define_functions.R")
+
+# Check correct R version, load groundhog package, and specify groundhog_day
+
+groundhog_day <- version_control()
+
+# No packages loaded
+
+# ---------------------------------------------------------------------------- #
+# Import network parameters and clean Qualtrics data  ----
+# ---------------------------------------------------------------------------- #
+
+# TODO: Refit network models after Phase I data cleaning is complete
+
+
+
+
+
+load("./02_networks/results/net_params/net_params_var_mlvar.RDS")
+
+# TODO: Finalize file organization after Phase I data cleaning is complete
+
+
+
+
+
+clean_qualtrics_dat_path <- "R:/MSS/Schleider_Lab/jslab/TRACK to TREAT/Data/Clean Data (Isaac)/"
+
+y_qualtrics_dat <- readRDS(paste0(clean_qualtrics_dat_path, "Phase 1 Youth Qualtrics Data.RDS"))
+p_qualtrics_dat <- readRDS(paste0(clean_qualtrics_dat_path, "Phase 1 Parent Qualtrics Data.RDS"))
+
+# ---------------------------------------------------------------------------- #
+# Merge datasets ----
+# ---------------------------------------------------------------------------- #
+
+# TODO
+
+
+
+
+
+
+
